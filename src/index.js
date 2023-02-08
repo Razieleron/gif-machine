496d04f03a0c6dacca26fa768db86ee805beed60
Jannon Sielaff <jannon.sielaff@gmail.com>
2023-02-08 15:26:54 -0800

    WORKING API CALL!
    Co-authored by: Molly Donegan <mollyrdonegan@gmail.com>
    Co-authored by: Vera Weikel <vera.weikel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -23,32 +23,17 @@ function printElements(fish) {
   document.querySelector('#where-the-api-info-goes').append(img);
 }
 
-// function printElements(fish) {
-//   let fishy = fish.data[0].images.downsized.url;//embed_url;//.images.looping.mp4; 
-//   // let p = document.querySelector("#where-the-api-info-goes");
-//   // let a = document.createAttribute("a", fishy); 
-
-
-//   document.querySelector('#where-the-api-info-goes').innerText = `${fishy}`;
-
-//   // p.append(a);
-  
-//   console.log("fish: ", fish);
- 
-//   console.log("fishy: ", fishy);
-// }
-
-function printError() {
-  //console.log("hi")
+function printError(error, fish) {
+  console.log("h im in printErrori");
+  console.error("printError(Err)");
+  document.querySelector('#where-the-api-info-goes').innerText = `There was an error getting ${fish}: ${error}`;
 }
 
 function userInputForm(event) {
   event.preventDefault();
   let fish = document.querySelector('#user-input').value;
   document.querySelector('#user-input').value = null;
-  // getGif(fish);
   getGif(fish);
-  printElements(fish);
 }
 
 window.addEventListener("load", function() {
--- src/js/giphy.js
+++ src/js/giphy.js
@@ -1,12 +1,8 @@
 export default class GifApiCall {  
   static async getGif(fish) {
     try {
-      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${fish}&limit=25&offset=0&rating=g&lang=en`);
+      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.PI_KEY}&q=${fish}&limit=25&offset=0&rating=g&lang=en`);
       const jsonifiedResponse = await response.json();
-      console.log(jsonifiedResponse);
-      //jsonifiedResponse.data.url or jsonifiedResponse.data.id for example
-
-
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText}
         ${jsonifiedResponse.message}`;
